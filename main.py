eefdbe0dc37cab80933c6a7f2fa8aca8d58a22a4
SaqibMaqsood-dev <saqibmaqsood441@gmail.com>
2025-12-03 13:31:31 +0500

    Proxy Server

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+import uvicorn
+from fastapi import FastAPI
+from routing import products
+
+app = FastAPI()
+app.include_router(products.router)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port",    type=int, default=3000)
+    args = parser.parse_args()
+
+    uvicorn.run("main:app", host="127.0.0.1", port=args.port, reload=True)
---
+++ routing/products.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI , Request , APIRouter
+from fastapi.responses import  JSONResponse
+from cache import cache
+
+
+
+import httpx
+router = APIRouter()
+
+
+origin = "https://dummyjson.com"
+
+
+router = APIRouter()
+
+@router.get("/products/{id}")
+async def get_products_by_id(id : int  ,  request : Request):
+    async with httpx.AsyncClient() as client :
+        cache_key = str(request.url.path)
+
+        if cache_key in cache:
+            return JSONResponse(content=cache[cache_key], headers={"X-CACHE": "HIT"})
+
+        url = origin + request.url.path
+        data = await  client.get(url)   
+        cache[cache_key] = data.json()
+        print(cache)
+        
+         # MISS → fetch from origin
+        
+        return JSONResponse(content=data.json() , headers={"X-CACHE" : "MISS"})
+    
